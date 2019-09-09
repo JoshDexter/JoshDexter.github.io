19239cb5cbd28e8265f82dfcc84fb71ecdb6cb42
JoshDexter <45461837+JoshDexter@users.noreply.github.com>
2019-09-09 14:11:33 -0600

    added line art

--- firstproject/sketch.js
+++ firstproject/sketch.js
@@ -9,30 +9,41 @@ let redAmount = 0;
 
 let redChangeAmount = 1;
 
+let brushSize = 200;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255, 255, 255);
 }
 
 function draw() {
- 
-  if (mouseButton === LEFT){
-    fill(redAmount, 0, 250);
-    noStroke()
-    ellipse(mouseX, mouseY, 100, 100);
+  //var y = event.deltaY;
 
-    redAmount += redChangeAmount;
-
-    if (redAmount >= 255){
-      redChangeAmount *= -1;
-    }
-    if (redAmount <= 0){
-      redChangeAmount *= -1;
-    }
-  } 
-  if (mouseButton === RIGHT){
-    fill(255, 255, 255);
-    noStroke()
-    ellipse(mouseX, mouseY, 100, 100);
+  if (mouseWheel(event.deltaY > 0)){
+    brushSize = brushSize - 1 ;
   }
+  if (mouseWheel(event.deltaY < 0)){
+    brushSize = brushSize + 1 ;
+  }
+  if (mouseIsPressed){
+    if (mouseButton === LEFT)
+      fill(redAmount, 0, 250);
+      noStroke()
+      ellipse(mouseX, mouseY, 100, 100);
+      redAmount += redChangeAmount;
+
+      if (redAmount >= 255){
+        redChangeAmount *= -1;
+      }
+      if (redAmount <= 0){
+        redChangeAmount *= -1;
+      }
+    } 
+    if (mouseButton === RIGHT){
+      fill(255, 255, 255);
+      noStroke()
+      ellipse(mouseX, mouseY, 100, 100);
+    }
 }
+
+mouse
