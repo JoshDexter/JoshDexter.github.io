b887be9bf8a03ce5870b8830d7067ca65c7953ae
JoshDexter <45461837+JoshDexter@users.noreply.github.com>
2019-09-12 12:23:30 -0600

    text

--- project/sketch.js
+++ project/sketch.js
@@ -1,4 +1,11 @@
+let input;
+
 function setup(){
-  createCanvas(500, 500)
+  createCanvas(windowWidth, windowHeight)
   background(255, 255, 255);
+  input = prompt("Hit or stand?");
+}
+function draw(){
+  rect(35, 700, 400, 40);
+  text(input, 40, 725, 30, 30);
 }
