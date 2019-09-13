d1740f256ee709bcf3f4c8289e06a865b5dd8faa
JoshDexter <45461837+JoshDexter@users.noreply.github.com>
2019-09-13 14:11:35 -0600

    black jack

--- lineart/sketch.js
+++ lineart/sketch.js
@@ -5,13 +5,32 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let card;
+let sizeW = 20;
+let sizeH = 30;
+
+function preload(){
+  card = loadImage("assets/playingcard.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255, 255, 255);
 }
 
 function draw() {
-  line(mouseX, mouseY, pmouseX, pmouseY);
+  background(255, 255, 255);
+  imageMode(CENTER)
+  image(card, mouseX, mouseY, sizeW, sizeH);
+  if (keyIsDown(38)){ 
+    sizeW += 10;
+    sizeH += 15; 
+  }
+  if (keyIsDown(40)){ 
+    sizeW -= 10;
+    sizeH -= 15; 
+  }
 }
+
+
+
 
--- project/sketch.js
+++ project/sketch.js
@@ -1,11 +1,37 @@
 let input;
+let bet;
+let dealerA;
+let dealerB;
+let yourHandA;
+let yourHandB;
+let yourHandT;
+let answer;
 
 function setup(){
   createCanvas(windowWidth, windowHeight)
   background(255, 255, 255);
-  input = prompt("Hit or stand?");
+  bet = prompt("enter bet");
+  textSize(25);
+  dealerA = round(random(1, 11));
+  dealerB = round(random(1, 11));
+  yourHandA = round(random(1, 11));
+  yourHandB =  round(random(1, 11));
+  draw();
+  hitorstand();
 }
 function draw(){
-  rect(35, 700, 400, 40);
-  text(input, 40, 725, 30, 30);
+  yourHandT = yourHandA + yourHandB
+  if (dealerA === 1){
+    dealerA = 11;
+  }
+  text(yourHandT, 750, 725)
+  text(bet, 750, 50)
+  rect(50, 700, 400, 40);
+  text(input, 50, 725);
+  text(dealerA, 50, 50);
+  
+}
+function hitorstand(){
+  answer = prompt("Hit or stand");
+  return answer;
 }
