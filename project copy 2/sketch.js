b4f3c52db4e7f6b6ba7d1dc069f723bb31198643
JoshDexter <45461837+JoshDexter@users.noreply.github.com>
2019-09-30 14:11:17 -0600

    click works

--- project copy 2/sketch.js
+++ project copy 2/sketch.js
@@ -5,6 +5,7 @@ let autoClickerSpeed = 0;
 let sausage;
 //let sausageLocationX = windowWidth/2;
 //let sausageLocationY = windowHeight/2;
+let buffer;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -22,12 +23,20 @@ function UI(){
   image(sausage, windowWidth/2, windowHeight/2);
   textSize(25);
   text(score, windowWidth/2, windowHeight/8)
+  rect(windowWidth/1.2, windowHeight/8, 100, 50);
 }
 function draw(){
   UI();
 }
-function mouseIsPressed(){
-  if (mouseX < windowWidth/2.3 && mouseX > windowWidth/1.7 && mouseY < windowWidth/3.1 && mouseY > windowWidth/1.5){
+function mousePressed(){
+  if (mouseY < windowHeight/1.7 && mouseY > windowHeight/2.3 && mouseX < windowWidth/1.5 && mouseX > windowWidth/3.1){
     score += 1;
+    console.log(score);
+    console.log(windowHeight)
+    console.log(windowWidth)
+    UI();  
+  }
+  if (mouseY > windowHeight/8 && mouseY < windowHeight/5 && mouseX > windowWidth/1.2 && mouseX < windowWidth/1.05){
+    console.log("AUTO")
   }
 }
