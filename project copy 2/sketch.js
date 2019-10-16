5761c79e6002caaa47f58841f3331fd90350b65d
JoshDexter <45461837+JoshDexter@users.noreply.github.com>
2019-10-16 14:08:58 -0600

    done

--- project copy 2/sketch.js
+++ project copy 2/sketch.js
@@ -40,6 +40,7 @@ function windowResized(){
 }
 function UI(){
   if (menuState === "game"){
+    createCanvas(windowWidth, windowHeight);
     background(255, 0, 0);
     imageMode(CENTER);
     image(bg, windowWidth/2, windowHeight/2, windowWidth, windowHeight)
@@ -53,7 +54,7 @@ function UI(){
     fill(255)
     rect(windowWidth/1.2, windowHeight/8, 100, 50);
     fill(0)
-    text(multiplierCost, windowWidth/1.2 + 35, windowHeight/6)
+    text(round(multiplierCost), windowWidth/1.2 + 35, windowHeight/6)
     fill(255, 0, 0)
     text("multiplier", windowWidth/1.2, windowHeight/8 - 5)
     fill(255)
@@ -64,30 +65,30 @@ function UI(){
     fill(255, 0, 0)
     text("auto click", windowWidth/1.2, windowHeight/4 - 5)
     fill(0)
-    if (hatState){
+    if (hatState === true){
       scale(0.15)
       if (hatSelected === someHat[1]){
-        image(hatSelected, width/0.36, height/0.34, 298*2, 246*2);
+        image(hatSelected, windowWidth/0.36, windowHeight/0.34, 298*2, 246*2);
       }
       else{
-        image(hatSelected, width/0.36, height/0.34);
+        image(hatSelected, windowWidth/0.36, windowHeight/0.34);
       } 
     }
   }
   else if (menuState === "hat"){
     background(255);
     fill(255);
-    rect(width/7.5, height/8, 100, 50)
-    rect(width/7.5 + 150, height/8, 100, 50)
-    rect(width/7.5 + 300, height/8, 100, 50)
-    rect(width/7.5 + 450, height/8, 100, 50)
-    rect(width/7.5 + 600, height/8, 100, 50)
-    image(som, width/7.5, height/8, 1286/18, 1002/18);
-    image(fedora, width/7.5 + 150, height/8, 298/5, 246/5);
-    image(cowboy, width/7.5 + 300, height/8, 913/15, 720/15);
-    image(sausage, width/7.5 + 499, height/8 + 25, 417/6, 190/6)
+    rect(windowWidth/7.5, windowHeight/8, 100, 50)
+    rect(windowWidth/7.5 + 150, windowHeight/8, 100, 50)
+    rect(windowWidth/7.5 + 300, windowHeight/8, 100, 50)
+    rect(windowWidth/7.5 + 450, windowHeight/8, 100, 50)
+    rect(windowWidth/7.5 + 600, windowHeight/8, 100, 50)
+    image(som, windowWidth/7.5, windowHeight/8, 1286/18, 1002/18);
+    image(fedora, windowWidth/7.5 + 150, windowHeight/8, 298/5, 246/5);
+    image(cowboy, windowWidth/7.5 + 300, windowHeight/8, 913/15, 720/15);
+    image(sausage, windowWidth/7.5 + 499, windowHeight/8 + 25, 417/6, 190/6)
     fill(0);
-    text("Back", width/7.5 + 620, height/8 + 35)
+    text("Back", windowWidth/7.5 + 620, windowHeight/8 + 35)
     fill(255);
   }
 }
@@ -106,25 +107,25 @@ function mousePressed(){
     if (mouseY < windowHeight/1.7 && mouseY > windowHeight/2.3 && mouseX < windowWidth/1.5 && mouseX > windowWidth/3.1){
       clickSound.play();
       score += multiplier;
-      // console.log(score);
-      // console.log(windowHeight)
-      // console.log(windowWidth)
       clickCounter += 1;
-      // console.log(clickCounter)
+
       
       UI();  
     }
     if (mouseY > windowHeight/8 && mouseY < windowHeight/5.7 && mouseX > windowWidth/1.2 && mouseX < windowWidth/1.05){
-      // console.log("AUTO")
+
       buffer = score;
-      if (buffer - multiplierCost >= 0){
-        score -= multiplierCost;
+      if (buffer - round(multiplierCost) >= 0){
+        if (multiplier > 7){
+          multiplier += 2;
+        }
+        score -= round(multiplierCost);
         multiplier += 1;
-        multiplierCost *= multiplier;
+        multiplierCost *= multiplier/1.5;
       }
     }
     if (mouseY > windowHeight/4.1 && mouseY < windowHeight/3.4 && mouseX > windowWidth/1.2 && mouseX < windowWidth/1.05){
-      // console.log("ye")
+     
       buffer = score;
       if (buffer - autoClickerCost >= 0){
         autoClicker = true;
@@ -137,26 +138,26 @@ function mousePressed(){
         }
       } 
     }
-    if (mouseY > height/2.755 && mouseY < height/2.35 && mouseX > width/1.2 && mouseX < width/1.05){ 
+    if (mouseY > windowHeight/2.755 && mouseY < windowHeight/2.35 && mouseX > windowWidth/1.2 && mouseX < windowWidth/1.05){ 
       menuState = "hat"; 
       hatState = true;
     }
 }
   if (menuState === "hat"){
-    if (mouseY > height/8 && mouseY < height/8 + 50 && mouseX > width/7.5 && mouseX < width/7.5 + 100){
+    if (mouseY > windowHeight/8 && mouseY < windowHeight/8 + 50 && mouseX > windowWidth/7.5 && mouseX < windowWidth/7.5 + 100){
       hatSelected = someHat[0];
     }
-    if (mouseY > height/8 && mouseY < height/8 + 50 && mouseX > width/7.5 + 150 && mouseX < width/7.5 + 100 + 150){
+    if (mouseY > windowHeight/8 && mouseY < windowHeight/8 + 50 && mouseX > windowWidth/7.5 + 150 && mouseX < windowWidth/7.5 + 100 + 150){
       hatSelected = someHat[1];
     }
-    if (mouseY > height/8 && mouseY < height/8 + 50 && mouseX > width/7.5 + 300 && mouseX < width/7.5 + 100 + 300){
+    if (mouseY > windowHeight/8 && mouseY < windowHeight/8 + 50 && mouseX > windowWidth/7.5 + 300 && mouseX < windowWidth/7.5 + 100 + 300){
       hatSelected = someHat[2];
     }
-    if (mouseY > height/8 && mouseY < height/8 + 50 && mouseX > width/7.5 + 450 && mouseX < width/7.5 + 100 + 450){
+    if (mouseY > windowHeight/8 && mouseY < windowHeight/8 + 50 && mouseX > windowWidth/7.5 + 450 && mouseX < windowWidth/7.5 + 100 + 450){
       hatState = false;
       UI();
     }
-    if (mouseY > height/8 && mouseY < height/8 + 50 && mouseX > width/7.5 + 600 && mouseX < width/7.5 + 100 + 600){
+    if (mouseY > windowHeight/8 && mouseY < windowHeight/8 + 50 && mouseX > windowWidth/7.5 + 600 && mouseX < windowWidth/7.5 + 100 + 600){
       menuState = "game";
     }
   }
